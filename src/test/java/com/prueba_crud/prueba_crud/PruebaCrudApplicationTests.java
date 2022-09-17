5f611cdf6c9003667f12e425d1df5b0d62c145c9
Luisa Torres <developer06@shareppy.com>
2022-09-17 10:26:20 -0500

    proyecto inicial

---
+++ src/test/java/com/prueba_crud/prueba_crud/PruebaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba_crud.prueba_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
